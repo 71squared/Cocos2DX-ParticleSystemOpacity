0ba386944b30c3b81e6cba260b2fc422852e6242
Tom Bradley <tom@nfhd.co.uk>
2014-08-21 15:27:05 +0100

    Initial commit

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,125 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = LabelTTF::create("Hello World", "Arial", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+
+    // add "HelloWorld" splash screen"
+    sprite = Sprite::create("HelloWorld.png");
+
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+    
+    
+    
+    // Schedule the update method
+    this->scheduleUpdate();
+    
+    
+    // Create the particle system
+    particle = ParticleSystemQuad::create("shadow.plist");
+    particle->retain();
+    particle->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    
+    // Create the render texture
+    rt = RenderTexture::create(visibleSize.width, visibleSize.height);
+    rt->retain();
+    
+    // Create a sprite using the texture so we can modify the opacity
+    auto rtSprite = Sprite::createWithTexture(rt->getSprite()->getTexture());
+    rtSprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    rtSprite->setFlippedY(true);
+    rtSprite->setOpacity(100);
+    this->addChild(rtSprite);
+    
+    return true;
+}
+
+void HelloWorld::update(float delta) {
+    
+    // update the particle system
+    particle->update(delta);
+    
+    // update the render texture
+    rt->beginWithClear(0, 0, 0, 0);
+    particle->visit();
+    rt->end();
+}
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
